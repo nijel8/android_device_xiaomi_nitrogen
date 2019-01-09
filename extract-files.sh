e221dfa164ad65cf6551cfcd9b5b7db5bc951556
nijel8 <nijel8@gmail.com>
2019-01-09 15:14:57 -0500

    Disable blobs manipulation for nitrogen for now

--- extract-files.sh
+++ extract-files.sh
@@ -60,49 +60,49 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+# DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
-sed -i \
-    's/\/system\/etc\//\/vendor\/etc\//g' \
-    "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
+# sed -i \
+#     's/\/system\/etc\//\/vendor\/etc\//g' \
+#     "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
 
 #
 # Use 8.1 libicuuc.so and libminikin.so for libMiCameraHal.so
 #
-ICUUC_V27="$DEVICE_BLOB_ROOT"/vendor/lib/libicuuc-v27.so
-MINIKIN_V27="$DEVICE_BLOB_ROOT"/vendor/lib/libminikin-v27.so
-patchelf --set-soname libicuuc-v27.so "$ICUUC_V27"
-patchelf --set-soname libminikin-v27.so "$MINIKIN_V27"
+# ICUUC_V27="$DEVICE_BLOB_ROOT"/vendor/lib/libicuuc-v27.so
+# MINIKIN_V27="$DEVICE_BLOB_ROOT"/vendor/lib/libminikin-v27.so
+# patchelf --set-soname libicuuc-v27.so "$ICUUC_V27"
+# patchelf --set-soname libminikin-v27.so "$MINIKIN_V27"
 
-MI_CAMERA_HAL="$DEVICE_BLOB_ROOT"/vendor/lib/libMiCameraHal.so
-patchelf --replace-needed libicuuc.so libicuuc-v27.so "$MI_CAMERA_HAL"
-patchelf --replace-needed libminikin.so libminikin-v27.so "$MI_CAMERA_HAL"
+# MI_CAMERA_HAL="$DEVICE_BLOB_ROOT"/vendor/lib/libMiCameraHal.so
+# patchelf --replace-needed libicuuc.so libicuuc-v27.so "$MI_CAMERA_HAL"
+# patchelf --replace-needed libminikin.so libminikin-v27.so "$MI_CAMERA_HAL"
 
 #
 # Remove unused linkage from camera.sdm660.so to avoid conflicts
 #
-CAMERA_SDM660="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
-patchelf --remove-needed libicuuc.so "$CAMERA_SDM660"
-patchelf --remove-needed libminikin.so "$CAMERA_SDM660"
+# CAMERA_SDM660="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
+# patchelf --remove-needed libicuuc.so "$CAMERA_SDM660"
+# patchelf --remove-needed libminikin.so "$CAMERA_SDM660"
 
-patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $DEVICE_BLOB_ROOT/vendor/lib/libvideorefiner.so
+#patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $DEVICE_BLOB_ROOT/vendor/lib/libvideorefiner.so
 
 "$MY_DIR"/setup-makefiles.sh
 
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+# DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
-CAMERA_HAL="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
+# CAMERA_HAL="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
 
-sed -i \
-    -e 's/\xe0\x6d\x01\x28\x0b\xd0/\x00\xbf\x00\xbf\x1f\xe0/' \
-    "$CAMERA_HAL"
+# sed -i \
+#     -e 's/\xe0\x6d\x01\x28\x0b\xd0/\x00\xbf\x00\xbf\x1f\xe0/' \
+#     "$CAMERA_HAL"
 
-patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$DEVICE_BLOB_ROOT"/vendor/bin/mlipayd
-patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libmlipay.so
+# patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$DEVICE_BLOB_ROOT"/vendor/bin/mlipayd
+# patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libmlipay.so
 
-sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_hw\/|\/data\/vendor\/modem_config\/mcfg_hw\/\x00\x00\x00\x00\x00\x00|g" \
-    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-hal-qmi.so
-sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_sw\/|\/data\/vendor\/modem_config\/mcfg_sw\/\x00\x00\x00\x00\x00\x00|g" \
-    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-hal-qmi.so
-sed -i "s|\/data\/vendor\/radio\/modem_config\/|\/data\/vendor\/modem_config\/\x00\x00\x00\x00\x00\x00|g" \
-    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-hal-qmi.so
+# sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_hw\/|\/data\/vendor\/modem_config\/mcfg_hw\/\x00\x00\x00\x00\x00\x00|g" \
+#     "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-hal-qmi.so
+# sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_sw\/|\/data\/vendor\/modem_config\/mcfg_sw\/\x00\x00\x00\x00\x00\x00|g" \
+#     "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-hal-qmi.so
+# sed -i "s|\/data\/vendor\/radio\/modem_config\/|\/data\/vendor\/modem_config\/\x00\x00\x00\x00\x00\x00|g" \
+#     "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-hal-qmi.so
