603e2be627a0424aeb2e1d2780046281277b13c6
nijel8 <nijel8@gmail.com>
2019-01-09 13:05:22 -0500

    Disable releasetools for now

--- releasetools.py
+++ releasetools.py
@@ -19,19 +19,19 @@ import common
 import re
 
 def FullOTA_Assertions(info):
-  AddModemAssertion(info)
+#  AddModemAssertion(info)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddModemAssertion(info)
+#  AddModemAssertion(info)
   return
 
 def AddModemAssertion(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
-  if m:
-    version = m.group(1).rstrip()
-    if len(version) and '*' not in version:
-      cmd = 'assert(nitrogen.verify_modem("' + version + '") == "1");'
-      info.script.AppendExtra(cmd)
+  # android_info = info.input_zip.read("OTA/android-info.txt")
+  # m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
+  # if m:
+  #   version = m.group(1).rstrip()
+  #   if len(version) and '*' not in version:
+  #     cmd = 'assert(nitrogen.verify_modem("' + version + '") == "1");'
+  #     info.script.AppendExtra(cmd)
   return
